75d4f891e44b8dbd187b2ef1770f955d940d0b51
nandu <nandakumars5352@gmail.com>
2026-03-10 09:54:22 +0530

    folder allignments

--- QA/React/react-practice/src/App.jsx
+++ QA/React/react-practice/src/App.jsx
@@ -2,28 +2,17 @@ import React from 'react'
 import Provider from './UseContext/One/Provider'
 import Child1 from './UseContext/One/Child1'
 import Child2 from './UseContext/One/Child2'
-import CounterReducer from './UseReducer/CounterReducer'
-import Parent from './DataTransfer/ToChild/Parent'
-import Stopwatch from './UseEffect/Stopwatch'
-import UseMemo from './UseMemo/UseMemo'
-import Fetch from './Others/Fetch'
-import FetchProducts from './Others/FetchProducts'
-import UsingUseFetch from './Others/UsingUseFetch'
-import Shop from './Others/SHOPCART/Shop'
 
 function App() {
   return (
 
     <>
-    <Shop/>
-    {/* <UsingUseFetch/> */}
-    {/* <Fetch/> */}
-    {/* <FetchProducts/> */}
-    {/* <UseMemo/> */}
-    {/* <Stopwatch/> */}
-    {/* <Parent/> */}
-    {/* <CounterReducer/> */}
+
     </>
+
+
+
+
 
 
 
